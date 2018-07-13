cab65794bea7120e18333a4ecd80630ac096b50b
takahiro-tsuruda <takahiro-tsuruda@cybozu.co.jp>
2018-07-13 13:28:00 +0900

    Add operation

--- operation.go
+++ operation.go
@@ -1,11 +1,25 @@
 package cke
 
-import "context"
+import (
+	"context"
+)
+
+// Commander is a single step to proceed an operation
+type Commander interface {
+	// Run executes the command
+	Run(ctx context.Context) error
+	// Command returns the command information
+	Command() Command
+}
 
 // Operator is an interface for operations
 type Operator interface {
 	// Name returns the operation name
 	Name() string
-	// Run executes the operation
-	Run(ctx context.Context) error
+	// NextCommand returns the next command or nil if completed
+	NextCommand() Commander
+	// NewRecord returns a new record for this operation
+	NewRecord() *Record
+	// Cleanup clean up garbage of previous failed operations, if any
+	Cleanup(ctx context.Context) error
 }
---
+++ strategy.go
@@ -0,0 +1,7 @@
+package cke
+
+// DecideToDo return next operation to do
+func DecideToDo(c *Cluster, cs *ClusterStatus) Operator {
+	// TODO
+	return nil
+}
