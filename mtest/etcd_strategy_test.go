39d70cd5f25ecdf182cabaa6d0edd58c088adf22
morimoto <kenji_morimoto@cybozu.co.jp>
2018-07-27 04:31:28 +0000

    [mtest] add etcd strategy multi-host test more.

--- mtest/etcd_strategy_test.go
+++ mtest/etcd_strategy_test.go
@@ -41,6 +41,40 @@ var _ = Describe("etcd strategy", func() {
 		Expect(status.NodeStatuses[node1].Etcd.HasData).To(BeTrue())
 	})
 
+	It("should remove unhealthy-and-non-control-plane node2 from etcd cluster, and destroy it's etcd", func() {
+		By("stopping etcd in node2")
+		execSafeAt(node2, "docker", "stop", "etcd")
+		execSafeAt(node2, "docker", "rm", "etcd")
+
+		By("Changing definition of node2")
+		ckecli("constraints", "set", "control-plane-count", "2")
+		cluster := getCluster()
+		cluster.Nodes[0].ControlPlane = true
+		cluster.Nodes[2].ControlPlane = true
+		ckecliClusterSet(cluster)
+
+		By("Checking cluster status")
+		Eventually(func() bool {
+			controlPlanes := []string{node1, node3}
+			workers := []string{node2, node4, node5, node6}
+			status, err := getClusterStatus()
+			if err != nil {
+				return false
+			}
+			defer status.Destroy()
+			return checkEtcdClusterStatus(status, controlPlanes, workers)
+		}).Should(BeTrue())
+
+		By("Checking that CKE removed worker node's data")
+		status, err := getClusterStatus()
+		Expect(err).NotTo(HaveOccurred())
+		defer status.Destroy()
+		Expect(status.NodeStatuses[node2].Etcd.HasData).To(BeFalse())
+	})
+
+	// unit test of etcd strategy contains a case of "start unstarted member",
+	// but that case is not here, because it is difficult to make "unstarted member"
+
 	It("should update node4 as control plane", func() {
 		By("Changing definition of node4")
 		ckecli("constraints", "set", "control-plane-count", "4")
@@ -61,5 +95,61 @@ var _ = Describe("etcd strategy", func() {
 			defer status.Destroy()
 			return checkEtcdClusterStatus(status, controlPlanes, workers)
 		}).Should(BeTrue())
+	})
+
+	It("should remove not-in-cluster node1 from etcd cluster", func() {
+		By("Removing definition of node1")
+		ckecli("constraints", "set", "control-plane-count", "2")
+		cluster := getCluster()
+		for i := 0; i < 3; i++ {
+			cluster.Nodes[i].ControlPlane = true
+		}
+		cluster.Nodes = cluster.Nodes[1:]
+		ckecliClusterSet(cluster)
+
+		By("Checking cluster status")
+		Eventually(func() bool {
+			controlPlanes := []string{node2, node3}
+			workers := []string{node4, node5, node6}
+			status, err := getClusterStatus()
+			if err != nil {
+				return false
+			}
+			defer status.Destroy()
+			return checkEtcdClusterStatus(status, controlPlanes, workers)
+		}).Should(BeTrue())
+
+		By("Checking that CKE did not remove non-cluster node's data")
+		status, err := getClusterStatus()
+		Expect(err).NotTo(HaveOccurred())
+		defer status.Destroy()
+		Expect(status.NodeStatuses[node1].Etcd.HasData).To(BeTrue())
+	})
+
+	It("should remove non-control-plane node2 from etcd cluster, and destroy it's etcd", func() {
+		By("Changing definition of node2")
+		ckecli("constraints", "set", "control-plane-count", "2")
+		cluster := getCluster()
+		cluster.Nodes[0].ControlPlane = true
+		cluster.Nodes[2].ControlPlane = true
+		ckecliClusterSet(cluster)
+
+		By("Checking cluster status")
+		Eventually(func() bool {
+			controlPlanes := []string{node1, node3}
+			workers := []string{node2, node4, node5, node6}
+			status, err := getClusterStatus()
+			if err != nil {
+				return false
+			}
+			defer status.Destroy()
+			return checkEtcdClusterStatus(status, controlPlanes, workers)
+		}).Should(BeTrue())
+
+		By("Checking that CKE removed worker node's data")
+		status, err := getClusterStatus()
+		Expect(err).NotTo(HaveOccurred())
+		defer status.Destroy()
+		Expect(status.NodeStatuses[node2].Etcd.HasData).To(BeFalse())
 	})
 })
