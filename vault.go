e4f927b465973e3a69d8995cd9aa4bd67c891641
takahiro-tsuruda <takahiro-tsuruda@cybozu.co.jp>
2018-08-29 12:03:31 +0900

    Create Vault client

---
+++ vault.go
@@ -0,0 +1,62 @@
+package cke
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"net"
+	"net/http"
+	"time"
+
+	vault "github.com/hashicorp/vault/api"
+)
+
+// VaultConfig is data to store in etcd
+type VaultConfig struct {
+	// Endpoint is the address of the Vault server.
+	Endpoint string `json:"endpoint"`
+
+	// CACert is x509 certificate in PEM format of the endpoint CA.
+	CACert string `json:"ca-cert"`
+
+	// RoleID is AppRole ID to login to Vault.
+	RoleID string `json:"role-id"`
+
+	// SecretID is AppRole secret to login to Vault.
+	SecretID string `json:"secret-id"`
+}
+
+// ConnectVault creates vault client
+func ConnectVault(c *VaultConfig) (*vault.Client, error) {
+	transport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+
+	if len(c.CACert) > 0 {
+		cp := x509.NewCertPool()
+		if !cp.AppendCertsFromPEM([]byte(c.CACert)) {
+			return nil, errors.New("invalid CA cert")
+		}
+
+		transport.TLSClientConfig = &tls.Config{
+			RootCAs:    cp,
+			MinVersion: tls.VersionTLS12,
+		}
+	}
+
+	return vault.NewClient(&vault.Config{
+		Address: c.Endpoint,
+		HttpClient: &http.Client{
+			Transport: transport,
+		},
+	})
+}
