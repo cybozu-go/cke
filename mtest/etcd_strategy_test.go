af8b08459f60aa424420627f4380d1e03240f31f
mitz <mitz@cybozu.co.jp>
2018-09-04 08:33:22 +0000

    Update mtest to avoid known bug
    The etcd go client fails if multiple https endpoints are specified when
    the client is initialised and the first etcd endpoint is unavailable.
    
    https://github.com/etcd-io/etcd/issues/9949

--- mtest/etcd_strategy_test.go
+++ mtest/etcd_strategy_test.go
@@ -12,23 +12,23 @@ import (
 var _ = Describe("etcd strategy", func() {
 	AfterEach(initializeControlPlane)
 
-	It("should remove unhealthy-and-not-in-cluster node1 from etcd cluster", func() {
-		By("Stopping etcd in node1")
-		execSafeAt(node1, "docker", "stop", "etcd")
-		execSafeAt(node1, "docker", "rm", "etcd")
+	It("should remove unhealthy-and-not-in-cluster node2 from etcd cluster", func() {
+		By("Stopping etcd in node2")
+		execSafeAt(node2, "docker", "stop", "etcd")
+		execSafeAt(node2, "docker", "rm", "etcd")
 
-		By("Removing definition of node1")
+		By("Removing definition of node2")
 		ckecli("constraints", "set", "control-plane-count", "2")
 		cluster := getCluster()
 		for i := 0; i < 3; i++ {
 			cluster.Nodes[i].ControlPlane = true
 		}
-		cluster.Nodes = cluster.Nodes[1:]
+		cluster.Nodes = append(cluster.Nodes[:1], cluster.Nodes[2:]...)
 		ckecliClusterSet(cluster)
 
 		By("Checking cluster status")
 		Eventually(func() bool {
-			controlPlanes := []string{node2, node3}
+			controlPlanes := []string{node1, node3}
 			workers := []string{node4, node5, node6}
 			status, err := getClusterStatus()
 			if err != nil {
@@ -40,7 +40,7 @@ var _ = Describe("etcd strategy", func() {
 		By("Checking that CKE did not remove non-cluster node's data")
 		status, err := getClusterStatus()
 		Expect(err).NotTo(HaveOccurred())
-		Expect(status.NodeStatuses[node1].Etcd.HasData).To(BeTrue())
+		Expect(status.NodeStatuses[node2].Etcd.HasData).To(BeTrue())
 	})
 
 	It("should remove unhealthy-and-non-control-plane node2 from etcd cluster, and destroy it's etcd", func() {
@@ -96,19 +96,19 @@ var _ = Describe("etcd strategy", func() {
 		}).Should(BeTrue())
 	})
 
-	It("should remove not-in-cluster node1 from etcd cluster", func() {
-		By("Removing definition of node1")
+	It("should remove not-in-cluster node2 from etcd cluster", func() {
+		By("Removing definition of node2")
 		ckecli("constraints", "set", "control-plane-count", "2")
 		cluster := getCluster()
 		for i := 0; i < 3; i++ {
 			cluster.Nodes[i].ControlPlane = true
 		}
-		cluster.Nodes = cluster.Nodes[1:]
+		cluster.Nodes = append(cluster.Nodes[:1], cluster.Nodes[2:]...)
 		ckecliClusterSet(cluster)
 
 		By("Checking cluster status")
 		Eventually(func() bool {
-			controlPlanes := []string{node2, node3}
+			controlPlanes := []string{node1, node3}
 			workers := []string{node4, node5, node6}
 			status, err := getClusterStatus()
 			if err != nil {
@@ -120,7 +120,7 @@ var _ = Describe("etcd strategy", func() {
 		By("Checking that CKE did not remove non-cluster node's data")
 		status, err := getClusterStatus()
 		Expect(err).NotTo(HaveOccurred())
-		Expect(status.NodeStatuses[node1].Etcd.HasData).To(BeTrue())
+		Expect(status.NodeStatuses[node2].Etcd.HasData).To(BeTrue())
 	})
 
 	It("should remove non-control-plane node2 from etcd cluster, and destroy it's etcd", func() {
@@ -148,14 +148,14 @@ var _ = Describe("etcd strategy", func() {
 		Expect(status.NodeStatuses[node2].Etcd.HasData).To(BeFalse())
 	})
 
-	It("should remove unhealthy node1 from etcd cluster and add node4 in appropriate order", func() {
-		By("Stopping etcd in node1 and changing definition of node1/node4 at once")
+	It("should remove unhealthy node2 from etcd cluster and add node4 in appropriate order", func() {
+		By("Stopping etcd in node2 and changing definition of node2/node4 at once")
 		stopCKE()
-		execSafeAt(node1, "docker", "stop", "etcd")
-		execSafeAt(node1, "docker", "rm", "etcd")
+		execSafeAt(node2, "docker", "stop", "etcd")
+		execSafeAt(node2, "docker", "rm", "etcd")
 		cluster := getCluster()
-		cluster.Nodes[0].ControlPlane = false
-		cluster.Nodes[1].ControlPlane = true
+		cluster.Nodes[0].ControlPlane = true
+		cluster.Nodes[1].ControlPlane = false
 		cluster.Nodes[2].ControlPlane = true
 		cluster.Nodes[3].ControlPlane = true
 		ckecliClusterSet(cluster)
@@ -163,8 +163,8 @@ var _ = Describe("etcd strategy", func() {
 
 		By("Checking cluster status")
 		Eventually(func() bool {
-			controlPlanes := []string{node2, node3, node4}
-			workers := []string{node1, node5, node6}
+			controlPlanes := []string{node1, node3, node4}
+			workers := []string{node2, node5, node6}
 			status, err := getClusterStatus()
 			if err != nil {
 				return false
