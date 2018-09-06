36ca50c70b55f61793d07cb1a3f7fb884d9b9e10
Shin'ya UEOKA <ueokande@i-beam.org>
2018-09-06 09:01:09 +0000

    Remove unused name from images

--- images.go
+++ images.go
@@ -18,7 +18,7 @@ func Image(name string) string {
 		return CKETools
 	case "tools":
 		return ToolsImage
-	case "hyperkube", "kube-apiserver", "kube-controller-manager", "kube-scheduler", "kubelet", "kube-proxy":
+	case "hyperkube":
 		return HyperkubeImage
 	case "pause":
 		return PauseImage
