137d099159fdcb32fb78989218facab2d0b98b04
takahiro-tsuruda <takahiro-tsuruda@cybozu.co.jp>
2018-09-25 15:06:10 +0900

    Use local etcdctl

--- mtest/ckecli_test.go
+++ mtest/ckecli_test.go
@@ -1,11 +1,14 @@
 package mtest
 
 import (
+	"bytes"
 	"encoding/json"
+	"os/exec"
 
 	"github.com/cybozu-go/cke/cli"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("ckecli", func() {
@@ -22,7 +25,19 @@ var _ = Describe("ckecli", func() {
 		c := localTempFile(res.Crt)
 		k := localTempFile(res.Key)
 		ca := localTempFile(res.CACrt)
-		_, _, err = etcdctl(host1, c.Name(), k.Name(), ca.Name(), "endpoint", "health")
-		Expect(err).NotTo(HaveOccurred())
+		etcdctlLocal(c.Name(), k.Name(), ca.Name(), "endpoint", "health")
 	})
 })
+
+func etcdctlLocal(crt, key, ca string, args ...string) []byte {
+	args = append([]string{"--endpoints=https://" + node1 + ":2379,https://" + node2 + ":2379,https://" + node3 + ":2379",
+		"--cert=" + crt, "--key=" + key, "--cacert=" + ca},
+		args...)
+	command := exec.Command("output/etcdctl", args...)
+	command.Env = append(command.Env, "ETCDCTL_API=3")
+	stdout := new(bytes.Buffer)
+	session, err := gexec.Start(command, stdout, GinkgoWriter)
+	Ω(err).ShouldNot(HaveOccurred())
+	Eventually(session).Should(gexec.Exit(0))
+	return stdout.Bytes()
+}
