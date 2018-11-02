fca955fa74bad2a8256869a97a302ce77b2c8e0b
takahiro-tsuruda <takahiro-tsuruda@cybozu.co.jp>
2018-11-02 17:58:18 +0900

    Add ClusterRole, ClusterRoleBinding and ServiceAccount

--- op/kube_coredns.go
+++ op/kube_coredns.go
@@ -5,14 +5,15 @@ import (
 
 	"github.com/cybozu-go/cke"
 	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type kubeCoreDNSCreateOp struct {
-	apiserver     *cke.Node
-	finished      bool
-	params        cke.KubeletParams
+	apiserver *cke.Node
+	finished  bool
+	params    cke.KubeletParams
 }
 
 // KubeCoreDNSCreateOp returns an Operator to create CoreDNS.
@@ -30,19 +31,18 @@ func (o *kubeCoreDNSCreateOp) NextCommand() cke.Commander {
 	}
 
 	o.finished = true
-	return createCoreDNSCommand{o.apiserver,  o.params}
+	return createCoreDNSCommand{o.apiserver, o.params}
 }
 
 type kubeCoreDNSUpdateOp struct {
-	apiserver     *cke.Node
-	finished      bool
-	params        cke.KubeletParams
+	apiserver *cke.Node
+	finished  bool
+	params    cke.KubeletParams
 }
 
 // KubeCoreDNSUpdateOp returns an Operator to update CoreDNS.
 func KubeCoreDNSUpdateOp() cke.Operator {
-	return &kubeCoreDNSCreateOp{
-	}
+	return &kubeCoreDNSCreateOp{}
 }
 
 func (o *kubeCoreDNSUpdateOp) Name() string {
@@ -55,12 +55,12 @@ func (o *kubeCoreDNSUpdateOp) NextCommand() cke.Commander {
 	}
 
 	o.finished = true
-	return updateCoreDNSCommand{o.apiserver,  o.params}
+	return updateCoreDNSCommand{o.apiserver, o.params}
 }
 
 type createCoreDNSCommand struct {
-	apiserver     *cke.Node
-	params        cke.KubeletParams
+	apiserver *cke.Node
+	params    cke.KubeletParams
 }
 
 func (c createCoreDNSCommand) Run(ctx context.Context, inf cke.Infrastructure) error {
@@ -70,10 +70,81 @@ func (c createCoreDNSCommand) Run(ctx context.Context, inf cke.Infrastructure) e
 	}
 
 	// ServiceAccount
+	accounts := cs.CoreV1().ServiceAccounts("kube-system")
+	_, err = accounts.Get(coreDNSAppName, metav1.GetOptions{})
+	switch {
+	case err == nil:
+	case errors.IsNotFound(err):
+		_, err = accounts.Create(&corev1.ServiceAccount{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      coreDNSAppName,
+				Namespace: "kube-system",
+			},
+		})
+		if err != nil {
+			return err
+		}
+	default:
+		return err
+	}
 
 	// ClusterRole
+	_, err = cs.RbacV1().ClusterRoles().Create(&rbacv1.ClusterRole{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: coreDNSRBACRoleName,
+			Labels: map[string]string{
+				"kubernetes.io/bootstrapping": "rbac-defaults",
+			},
+		},
+		Rules: []rbacv1.PolicyRule{
+			{
+				APIGroups: []string{""},
+				Resources: []string{
+					"endpoints",
+					"services",
+					"pods",
+					"namespaces",
+				},
+				Verbs: []string{
+					"list",
+					"watch",
+				},
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
 
 	// ClusterRoleBinding
+	_, err = cs.RbacV1().ClusterRoleBindings().Create(&rbacv1.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: coreDNSRBACRoleName,
+			Labels: map[string]string{
+				"kubernetes.io/bootstrapping": "rbac-defaults",
+			},
+			Annotations: map[string]string{
+				// turn on auto-reconciliation
+				// https://kubernetes.io/docs/reference/access-authn-authz/rbac/#auto-reconciliation
+				rbacv1.AutoUpdateAnnotationKey: "true",
+			},
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "ClusterRole",
+			Name:     coreDNSRBACRoleName,
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind:      rbacv1.ServiceAccountKind,
+				Name:      coreDNSAppName,
+				Namespace: "kube-system",
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
 
 	// ConfigMap
 
@@ -132,8 +203,8 @@ func (c createCoreDNSCommand) Command() cke.Command {
 }
 
 type updateCoreDNSCommand struct {
-	apiserver     *cke.Node
-	params        cke.KubeletParams
+	apiserver *cke.Node
+	params    cke.KubeletParams
 }
 
 func (c updateCoreDNSCommand) Run(ctx context.Context, inf cke.Infrastructure) error {
