e6c8b61af41a3dc852582fb49f556cbe5b3a6997
mitz <mitz@cybozu.co.jp>
2018-11-05 03:07:31 +0000

    Add ConfigMaps

--- op/kube_coredns.go
+++ op/kube_coredns.go
@@ -127,8 +127,159 @@ func (c createCoreDNSCommand) Run(ctx context.Context, inf cke.Infrastructure) e
 	}
 
 	// ConfigMap
+	configs := cs.CoreV1().ConfigMaps("kube-system")
+	_, err = configs.Get(coreDNSAppName, metav1.GetOptions{})
+	switch {
+	case err == nil:
+	case errors.IsNotFound(err):
+		_, err = configs.Create(&corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      coreDNSAppName,
+				Namespace: "kube-system",
+			},
+			Data: map[string]string{
+				"Corefile": `.:53 {
+    errors
+    health
+    kubernetes cluster.local in-addr.arpa ip6.arpa {
+      pods verified
+      upstream
+      fallthrough in-addr.arpa ip6.arpa
+    }
+    proxy . /etc/resolv.conf
+    cache 30
+    loop
+    reload
+    loadbalance
+}
+`,
+			},
+		})
+		if err != nil {
+			return err
+		}
+	default:
+		return err
+	}
 
 	// Deployment
+	//	deployments := cs.AppsV1().Deployments("kube-system")
+	//	_, err = deployments.Get(coreDNSAppName, metav1.GetOptions{})
+	//	switch {
+	//	case err == nil:
+	//	case errors.IsNotFound(err):
+	//		_, err = deployments.Create(&appsv1.Deployment{
+	//			ObjectMeta: metav1.ObjectMeta{
+	//				Name:      coreDNSAppName,
+	//				Namespace: "kube-system",
+	//				Labels: map[string]string{
+	//					"k8s-app":            coreDNSAppName,
+	//					"kubernetes.io/name": "CoreDNS",
+	//				},
+	//			},
+	//			Spec: appsv1.DeploymentSpec{
+	//				Replicas: int32ToPtr(2),
+	//				Strategy: appsv1.DeploymentStrategy{
+	//					Type: appsv1.RollingUpdateDeploymentStrategyType,
+	//					RollingUpdate: &appsv1.RollingUpdateDeployment{
+	//						MaxUnavailable: intstr.FromInt(1),
+	//					},
+	//				},
+	//			},
+	//			Selector: &metav1.LabelSelector{
+	//				MatchLabels: map[string]string{
+	//					"k8s-app": coreDNSAppName,
+	//				},
+	//			},
+	//			Template: corev1.PodTemplateSpec{
+	//				ObjectMeta: metav1.ObjectMeta{
+	//					Labels: map[string]string{
+	//						"k8s-app": coreDNSAppName,
+	//					},
+	//				},
+	//				Spec: corev1.PodSpec{
+	//					ServiceAccountName: coreDNSAppName,
+	//					Tolerations: []corev1.Toleration{
+	//						{
+	//							Key:    "node-role.kubernetes.io/master",
+	//							Effect: TaintEffectNoSchedule,
+	//						},
+	//						{
+	//							Key:      "CriticalAddonsOnly",
+	//							Operator: TolerationOpExists,
+	//						},
+	//					},
+	//					Containers: []corev1.Container{
+	//						Name:            coreDNSAppName,
+	//						Image:           cke.CoreDNSImage,
+	//						ImagePullPolicy: PullIfNotPresent,
+	//						Resources: corev1.ResourceRequirements{
+	//							Limits: corev1.ResourceList{
+	//								corev1.ResourceMemory: resource.MustParse("170Mi"),
+	//							},
+	//							Requests: corev1.ResourceList{
+	//								corev1.ResourceCPU:    resource.MustParse("100m"),
+	//								corev1.ResourceMemory: resource.MustParse("70Mi"),
+	//							},
+	//						},
+	//						Args: []string{"-conf", "/etc/coredns/Corefile"},
+	//						VolumeMounts: []corev1.VolumeMount{
+	//							Name:      "config-volume",
+	//							MountPath: "/etc/coredns",
+	//							ReadOnly:  true,
+	//						},
+	//						Ports: []corev1.ContainerPort{
+	//							{
+	//								ContainerPort: 53,
+	//								Name:          "dns",
+	//								Protocol:      corev1.ProtocolUDP,
+	//							},
+	//							{
+	//								ContainerPort: 53,
+	//								Name:          "dns-tcp",
+	//								Protocol:      corev1.ProtocolTCP,
+	//							},
+	//						},
+	//						SecurityContext: &corev1.SecurityContext{
+	//							AllowPrivilegeEscalation: false,
+	//							Capabilities: &corev1.Capabilities{
+	//								Add:  []corev1.Capacity{"NET_BIND_SERVICE"},
+	//								Drop: []corev1.Capacity{"all"},
+	//							},
+	//							ReadOnlyRootFilesystem: true,
+	//						},
+	//						LivenessProbe: &corev1.Probe{
+	//							HTTPGet: &corev1.HTTPGetAction{
+	//								Path:   "/health",
+	//								Port:   intstr.FromInt(8080),
+	//								Scheme: corev1.URISchemeHTTP,
+	//							},
+	//							InitialDelaySeconds: 60,
+	//							TimeoutSeconds:      5,
+	//							SuccessThreshold:    1,
+	//							FailureThreshold:    5,
+	//						},
+	//					},
+	//					DNSPolicy: corev1.DNSDefault,
+	//					Volumes: []corev1.Volume{
+	//						Name: "config-volume",
+	//						ConfigMap: &corev1.ConfigMapVolumeSource{
+	//							Name: coreDNSAppName,
+	//							Items: []corev1.KeyToPath{
+	//								Key:  "Corefile",
+	//								Path: "Corefile",
+	//							},
+	//						},
+	//					},
+	//				},
+	//			},
+	//		})
+	//		if err != nil {
+	//			return err
+	//		}
+	//	default:
+	//		return err
+	//	}
 
 	// Service
 	services := cs.CoreV1().Services("kube-system")
@@ -180,4 +331,8 @@ func (c createCoreDNSCommand) Command() cke.Command {
 		Name:   "createCoreDNSCommand",
 		Target: "kube-system",
 	}
+}
+
+func int32ToPtr(i int32) *int32 {
+	return &i
 }
