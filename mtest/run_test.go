39bd0cc71db49d6b940143bb1e825775dda8bb4b
Shin'ya UEOKA <ueokande@i-beam.org>
2018-11-15 23:29:15 +0000

    [mtest] install apps to /opt/bin

--- mtest/run_test.go
+++ mtest/run_test.go
@@ -141,7 +141,7 @@ func runCKE() error {
 				return err
 			}
 			defer sess.Close()
-			return sess.Run("sudo systemd-run --unit=cke.service --setenv=GOFAIL_HTTP=0.0.0.0:1234 /data/cke -interval 3s -session-ttl 5s")
+			return sess.Run("sudo systemd-run --unit=cke.service --setenv=GOFAIL_HTTP=0.0.0.0:1234 /opt/bin/cke -interval 3s -session-ttl 5s")
 		})
 	}
 	env.Stop()
