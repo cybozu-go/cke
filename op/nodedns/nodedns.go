95294b5131ca2f7d1ce5501b3fedddc4974b3b64
Hiroshi Muraoka <hiroshi-muraoka@cybozu.co.jp>
2019-04-19 06:10:24 +0000

    Update node label reference for beta.kubernetes.io/os
    Now it is kubernetes.io/os

--- op/nodedns/nodedns.go
+++ op/nodedns/nodedns.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"text/template"
 
-	"github.com/cybozu-go/cke"
 	"github.com/cybozu-go/cke/op"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -56,89 +55,6 @@ forward-zone:
   forward-addr: {{ . }}
   {{- end }}
 {{- end }}
-`
-
-// UnboundTemplateVersion is the version of unbound template
-const UnboundTemplateVersion = "1"
-
-var unboundDaemonSetText = `
-metadata:
-  name: node-dns
-  namespace: kube-system
-  annotations:
-    cke.cybozu.com/image: ` + cke.UnboundImage.Name() + `
-    cke.cybozu.com/template-version: ` + UnboundTemplateVersion + `
-spec:
-  selector:
-    matchLabels:
-      cke.cybozu.com/appname: node-dns
-  updateStrategy:
-    type: RollingUpdate
-    rollingUpdate:
-      maxUnavailable: 1
-  template:
-    metadata:
-      labels:
-        cke.cybozu.com/appname: node-dns
-    spec:
-      priorityClassName: system-node-critical
-      nodeSelector:
-        beta.kubernetes.io/os: linux
-      hostNetwork: true
-      tolerations:
-        # Make sure unbound gets scheduled on all nodes.
-        - effect: NoSchedule
-          operator: Exists
-        # Mark the pod as a critical add-on for rescheduling.
-        - key: CriticalAddonsOnly
-          operator: Exists
-        - effect: NoExecute
-          operator: Exists
-      terminationGracePeriodSeconds: 0
-      containers:
-        - name: unbound
-          image: ` + cke.UnboundImage.Name() + `
-          args:
-            - -c
-            - /etc/unbound/unbound.conf
-          securityContext:
-            allowPrivilegeEscalation: false
-            capabilities:
-              add:
-              - NET_BIND_SERVICE
-              drop:
-              - all
-            readOnlyRootFilesystem: true
-          livenessProbe:
-            tcpSocket:
-              port: 53
-              host: localhost
-            periodSeconds: 1
-            initialDelaySeconds: 1
-            failureThreshold: 6
-          volumeMounts:
-            - name: config-volume
-              mountPath: /etc/unbound
-        - name: reload
-          image: ` + cke.UnboundImage.Name() + `
-          command:
-          - /usr/local/bin/reload-unbound
-          securityContext:
-            allowPrivilegeEscalation: false
-            capabilities:
-              drop:
-              - all
-            readOnlyRootFilesystem: true
-          volumeMounts:
-            - name: config-volume
-              mountPath: /etc/unbound
-      volumes:
-        - name: config-volume
-          configMap:
-            name: node-dns
-            items:
-            - key: unbound.conf
-              path: unbound.conf
 `
 
 // ConfigMap returns ConfigMap for unbound daemonset
