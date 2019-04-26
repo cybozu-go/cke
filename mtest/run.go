7cad3cac24238aa0232cbd431b0b650e38e844c5
Satoru Takeuchi <sat@cybozu.co.jp>
2019-04-26 00:29:08 +0000

    fix

--- mtest/run.go
+++ mtest/run.go
@@ -221,7 +221,7 @@ func runCKE(image string) error {
 			}
 			defer sess.Close()
 			return sess.Run("sudo mkdir -p /var/lib/cke && sudo systemd-run --unit=cke.service " +
-				"--setenv=GOFAIL_HTTP=0.0.0.0:1234 " +
+				"--setenv=GOFAIL_HTTP=0.0.0.0:1234 -p 1234:1234" +
 				"docker run --rm --network=host --name cke " +
 				"--mount type=bind,source=/var/lib/cke,target=/var/lib/cke " +
 				"--mount type=bind,source=/etc/cke/,target=/etc/cke/ " +
