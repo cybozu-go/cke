3ce0d83787a32ac962b434c65ab0162fac350268
mitz <mitz@cybozu.co.jp>
2019-07-09 09:24:01 +0900

    [mtest] Add --certs-gc-interval 10s to check tidy is running

--- mtest/run.go
+++ mtest/run.go
@@ -220,7 +220,7 @@ func runCKE(image string) error {
 				"-e GOFAIL_HTTP=0.0.0.0:1234 " +
 				"--mount type=bind,source=/var/lib/cke,target=/var/lib/cke " +
 				"--mount type=bind,source=/etc/cke/,target=/etc/cke/ " +
-				image + " --config /etc/cke/cke.yml --interval 3s --session-ttl 5s --loglevel debug")
+				image + " --config /etc/cke/cke.yml --interval 3s --certs-gc-interval 10s --session-ttl 5s --loglevel debug")
 		})
 	}
 	env.Stop()
