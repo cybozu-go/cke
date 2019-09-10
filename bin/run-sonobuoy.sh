a00cc876ef52751d76c075fee5abb8574775449f
ymmt <ymmt2005@gmail.com>
2019-09-10 15:49:30 +0000

    circleci: run sonobuoy in CI
    Since running sonobuoy takes too long, it requires manual
    approval to run for a branch.
    
    For tags, sonobuoy is always run and the result will be
    released to GitHub.

---
+++ bin/run-sonobuoy.sh
@@ -0,0 +1,63 @@
+#!/bin/sh -ex
+
+. $(dirname $0)/env-sonobuoy
+
+delete_instance() {
+  if [ $RET -ne 0 ]; then
+    # do not delete GCP instance upon test failure to help debugging.
+    return
+  fi
+  $GCLOUD compute instances delete ${INSTANCE_NAME} --zone ${ZONE} || true
+}
+
+# Create GCE instance
+$GCLOUD compute instances delete ${INSTANCE_NAME} --zone ${ZONE} || true
+$GCLOUD compute instances create ${INSTANCE_NAME} \
+  --zone ${ZONE} \
+  --machine-type ${MACHINE_TYPE} \
+  --image vmx-enabled \
+  --boot-disk-type ${DISK_TYPE} \
+  --boot-disk-size ${BOOT_DISK_SIZE}
+
+RET=0
+trap delete_instance INT QUIT TERM 0
+
+for i in $(seq 300); do
+  if $GCLOUD compute ssh --zone=${ZONE} cybozu@${INSTANCE_NAME} --command=date 2>/dev/null; then
+    break
+  fi
+  sleep 1
+done
+
+cat >run.sh <<EOF
+#!/bin/sh -e
+
+# Run mtest
+GOPATH=\$HOME/go
+export GOPATH
+GO111MODULE=on
+export GO111MODULE
+PATH=/usr/local/go/bin:\$GOPATH/bin:\$PATH
+export PATH
+
+git clone https://github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} \
+    \$HOME/go/src/github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}
+cd \$HOME/go/src/github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}
+git checkout -qf ${CIRCLE_SHA1}
+
+cd sonobuoy
+make setup
+make run
+exec make sonobuoy
+EOF
+chmod +x run.sh
+
+$GCLOUD compute scp --zone=${ZONE} run.sh cybozu@${INSTANCE_NAME}:
+set +e
+$GCLOUD compute ssh --zone=${ZONE} cybozu@${INSTANCE_NAME} --command='sudo -H /home/cybozu/run.sh'
+RET=$?
+if [ "$RET" -eq 0 ]; then
+  $GCLOUD compute scp --zone=${ZONE} cybozu@${INSTANCE_NAME}:/home/cybozu/go/src/github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/sonobuoy/sonobuoy.tar.gz /tmp/sonobuoy.tar.gz
+fi
+
+exit $RET
