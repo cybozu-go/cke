2a38b3fc29b4e813f44a95a6445ca28bae6b954e
morimoto-cybozu <kenji_morimoto@cybozu.co.jp>
2019-10-04 07:51:39 +0000

    mtest: stop CKE before halting node.

--- mtest/robustness.go
+++ mtest/robustness.go
@@ -2,12 +2,23 @@ package mtest
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 // TestStopCP stops 1 control plane for succeeding tests
 func TestStopCP() {
 	It("should stop CP", func() {
-		execAt(node2, "sudo", "systemd-run", "poweroff", "-f", "-f")
+		// stop CKE temporarily to avoid hang-up in SSH session due to node2 shutdown
+		stopCKE()
+
+		execAt(node2, "sudo", "systemd-run", "halt", "-f", "-f")
+		Eventually(func() error {
+			_, _, err := execAt(node2, "true")
+			return err
+		}).ShouldNot(Succeed())
+
 		execAt(node3, "sudo", "systemctl", "stop", "sshd.socket")
+
+		runCKE(ckeImageURL)
 	})
 }
