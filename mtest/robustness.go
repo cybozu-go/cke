12fde6e1d7e75c576dd65b3a586a7c3a91bfa627
binoue <banji-inoue@cybozu.co.jp>
2019-10-04 15:15:28 +0900

    fix

--- mtest/robustness.go
+++ mtest/robustness.go
@@ -7,7 +7,7 @@ import (
 // TestStopCP stops 1 control plane for succeeding tests
 func TestStopCP() {
 	It("should stop CP", func() {
-		execAt(node2, "sudo", "systemd-run", "halt", "-f", "-f")
+		execAt(node2, "sudo", "systemd-run", "poweroff", "-f", "-f")
 		execAt(node3, "sudo", "systemctl", "stop", "sshd.socket")
 	})
 }
