7577566e04cecd983a57609800c302af95527212
masa213f <masa213f@gmail.com>
2019-12-16 02:34:41 +0000

    Update CoreDNS to v1.6.6

--- images.go
+++ images.go
@@ -14,7 +14,7 @@ const (
 	HyperkubeImage = Image("quay.io/cybozu/hyperkube:1.16.3.1")
 	ToolsImage     = Image("quay.io/cybozu/cke-tools:1.6.3")
 	PauseImage     = Image("quay.io/cybozu/pause:3.1.0.3")
-	CoreDNSImage   = Image("quay.io/cybozu/coredns:1.6.5.1")
+	CoreDNSImage   = Image("quay.io/cybozu/coredns:1.6.6.1")
 	UnboundImage   = Image("quay.io/cybozu/unbound:1.9.5.1")
 )
 
