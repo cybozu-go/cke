18171b7b8f9530fee4b80056dcda369d7ce8ce8e
zoetrope <a.ikezoe@gmail.com>
2020-01-20 16:59:17 +0900

    [ci skip][wip] run sonobuoy on GCP instances instead of Vagrant

--- bin/run-sonobuoy.sh
+++ bin/run-sonobuoy.sh
@@ -7,45 +7,50 @@ delete_instance() {
     # do not delete GCP instance upon test failure to help debugging.
     return
   fi
-  $GCLOUD compute instances delete ${INSTANCE_NAME} --zone ${ZONE} || true
+  for i in $(seq 0 3); do
+    $GCLOUD compute instances delete ${INSTANCE_NAME}-${i} --zone ${ZONE} || true
+  done
 }
 
-# Create GCE instance
-$GCLOUD compute instances delete ${INSTANCE_NAME} --zone ${ZONE} || true
-$GCLOUD compute instances create ${INSTANCE_NAME} \
+$GCLOUD compute instances delete ${INSTANCE_NAME}-0 --zone ${ZONE} || true
+$GCLOUD compute instances create ${INSTANCE_NAME}-0 \
   --zone ${ZONE} \
   --machine-type ${MACHINE_TYPE} \
   --image vmx-enabled \
   --boot-disk-type ${DISK_TYPE} \
-  --boot-disk-size ${BOOT_DISK_SIZE} \
-  --local-ssd interface=nvme \
-  --local-ssd interface=nvme \
-  --local-ssd interface=nvme \
-  --local-ssd interface=nvme
+  --boot-disk-size ${BOOT_DISK_SIZE}
+
+for i in $(seq 3); do
+  $GCLOUD compute instances delete ${INSTANCE_NAME}-${i} --zone ${ZONE} || true
+  $GCLOUD compute instances create ${INSTANCE_NAME}-${i} \
+    --zone ${ZONE} \
+    --machine-type ${MACHINE_TYPE} \
+    --image-project coreos-cloud \
+    --image-family coreos-stable \
+    --boot-disk-type ${DISK_TYPE} \
+    --boot-disk-size ${BOOT_DISK_SIZE}
+done
 
 RET=0
 trap delete_instance INT QUIT TERM 0
 
-for i in $(seq 300); do
-  if $GCLOUD compute ssh --zone=${ZONE} cybozu@${INSTANCE_NAME} --command=date 2>/dev/null; then
-    break
-  fi
-  sleep 1
+for i in $(seq 0 3); do
+  for i in $(seq 300); do
+    if $GCLOUD compute ssh --zone=${ZONE} core@${INSTANCE_NAME}-${i} --command=date 2>/dev/null; then
+      break
+    fi
+    sleep 1
+  done
 done
 
 # Extend instance life to complete sonobuoy test
-$GCLOUD compute instances add-metadata ${INSTANCE_NAME} --zone ${ZONE} \
-  --metadata extended=$(date -Iseconds -d+4hours)
+for i in $(seq 0 3); do
+  $GCLOUD compute instances add-metadata ${INSTANCE_NAME}-${i} --zone ${ZONE} \
+    --metadata extended=$(date -Iseconds -d+4hours)
+done
 
 cat >run.sh <<EOF
 #!/bin/sh -ex
-
-# mkfs and mount local SSD on /var/scratch
-mkfs -t ext4 -F /dev/nvme0n1
-mkdir -p /var/scratch
-mount -t ext4 /dev/nvme0n1 /var/scratch
-chmod 1777 /var/scratch
-mkdir /var/scratch/vbox /var/scratch/vagrant
 
 # Run sonobuoy
 GOPATH=\$HOME/go
@@ -54,8 +59,6 @@ GO111MODULE=on
 export GO111MODULE
 PATH=/usr/local/go/bin:\$GOPATH/bin:\$PATH
 export PATH
-ln -s /var/scratch/vbox \$HOME/'VirtualBox VMs'
-ln -s /var/scratch/vagrant \$HOME/.vagrant.d
 
 git clone https://github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} \
     \$HOME/go/src/github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}
@@ -70,12 +73,22 @@ mv sonobuoy.tar.gz /tmp
 EOF
 chmod +x run.sh
 
-$GCLOUD compute scp --zone=${ZONE} run.sh cybozu@${INSTANCE_NAME}:
+WORKER1_ADDRESS=$($GCLOUD compute instances describe ${INSTANCE_NAME}-1 --zone ${ZONE} --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
+WORKER2_ADDRESS=$($GCLOUD compute instances describe ${INSTANCE_NAME}-2 --zone ${ZONE} --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
+WORKER3_ADDRESS=$($GCLOUD compute instances describe ${INSTANCE_NAME}-3 --zone ${ZONE} --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
+
+sed -e "s|@WORKER1_ADDRESS@|${WORKER1_ADDRESS}|" \
+  -e "s|@WORKER2_ADDRESS@|${WORKER2_ADDRESS}|" \
+  -e "s|@WORKER3_ADDRESS@|${WORKER3_ADDRESS}|" $(dirname $0)/../sonobuoy/cke-cluster.yml.template > cke-cluster.yml
+
+$GCLOUD compute scp --zone=${ZONE} run.sh cybozu@${INSTANCE_NAME}-0:
+$GCLOUD compute scp --zone=${ZONE} cke-cluster.yml cybozu@${INSTANCE_NAME}-0:
+$GCLOUD compute scp --zone=${ZONE} gcp.private-key cybozu@${INSTANCE_NAME}-0:
 set +e
-$GCLOUD compute ssh --zone=${ZONE} cybozu@${INSTANCE_NAME} --command='sudo -H /home/cybozu/run.sh'
+$GCLOUD compute ssh --zone=${ZONE} cybozu@${INSTANCE_NAME}-0 --command='sudo -H /home/cybozu/run.sh'
 RET=$?
 if [ "$RET" -eq 0 ]; then
-  $GCLOUD compute scp --zone=${ZONE} cybozu@${INSTANCE_NAME}:/tmp/sonobuoy.tar.gz /tmp/sonobuoy.tar.gz
+  $GCLOUD compute scp --zone=${ZONE} cybozu@${INSTANCE_NAME}-0:/tmp/sonobuoy.tar.gz /tmp/sonobuoy.tar.gz
 fi
 
 exit $RET
