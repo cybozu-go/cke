207211c5fbf072bb3d9513bfedaac19949a11f49
Yuji Ito <llamerada.jp@gmail.com>
2020-03-13 04:59:12 +0000

    update container images version
    Signed-off-by: Yuji Ito <llamerada.jp@gmail.com>

--- images.go
+++ images.go
@@ -11,7 +11,7 @@ func (i Image) Name() string {
 // Container image definitions
 const (
 	EtcdImage       = Image("quay.io/cybozu/etcd:3.3.18.1")
-	KubernetesImage = Image("quay.io/cybozu/kubernetes:1.17.3.1")
+	KubernetesImage = Image("quay.io/cybozu/kubernetes:1.17.4.1")
 	ToolsImage      = Image("quay.io/cybozu/cke-tools:1.6.7")
 	PauseImage      = Image("quay.io/cybozu/pause:3.2.0.1")
 	CoreDNSImage    = Image("quay.io/cybozu/coredns:1.6.7.1")
