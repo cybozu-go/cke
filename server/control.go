4a9c5b755584d78c06ce6ee673599bb5fdd75fe5
Daichi Sakaue <daichi-sakaue@cybozu.co.jp>
2020-12-07 15:07:05 +0900

    reboot: reflect review comments 2
    Signed-off-by: Daichi Sakaue <daichi-sakaue@cybozu.co.jp>

--- server/control.go
+++ server/control.go
@@ -266,18 +266,19 @@ func (c Controller) runOnce(ctx context.Context, leaderKey string, tick <-chan t
 		return err
 	}
 
-	var reboot *cke.RebootQueueEntry
-	disabled, err := inf.Storage().IsRebootQueueDisabled(ctx)
+	re, err := inf.Storage().GetRebootsEntries(ctx)
 	if err != nil {
 		return err
 	}
-	if !disabled {
-		re, err := inf.Storage().GetRebootsEntries(ctx)
+	metrics.UpdateReboot(len(re))
+
+	var reboot *cke.RebootQueueEntry
+	if len(re) > 0 {
+		disabled, err := inf.Storage().IsRebootQueueDisabled(ctx)
 		if err != nil {
 			return err
 		}
-		metrics.UpdateReboot(len(re))
-		if len(re) > 0 {
+		if !disabled {
 			reboot = re[0]
 		}
 	}
