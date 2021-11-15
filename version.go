a8b5ceaf0f33d2c3dbcac2f6040b7a8237fc2ba4
morimoto-cybozu <kenji_morimoto@cybozu.co.jp>
2021-11-15 10:52:24 +0000

    Bump version to 1.21.2

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package cke
 
 // Version represents current cke version
-const Version = "1.21.1"
+const Version = "1.21.2"
 
 // ConfigVersion represents the current configuration scheme
 // of how CKE constructs its Kubernetes cluster.
