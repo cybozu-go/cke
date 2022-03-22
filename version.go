58aa557cf2568a12f451a591f079710dcae5099b
zoetrope <a.ikezoe@gmail.com>
2022-03-23 04:51:36 +0900

    Bump version to 1.22.2

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package cke
 
 // Version represents current cke version
-const Version = "1.22.1"
+const Version = "1.22.2"
 
 // ConfigVersion represents the current configuration scheme
 // of how CKE constructs its Kubernetes cluster.
