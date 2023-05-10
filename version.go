dd5b5ecf587ac30d78325fea6f0a83f14d1fb6e1
zeroalphat <taichi-takemura@cybozu.co.jp>
2023-05-10 00:58:59 +0000

    Bump version to 1.25.2
    Signed-off-by: zeroalphat <taichi-takemura@cybozu.co.jp>

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package cke
 
 // Version represents current cke version
-const Version = "1.25.1"
+const Version = "1.25.2"
 
 // ConfigVersion represents the current configuration scheme
 // of how CKE constructs its Kubernetes cluster.
