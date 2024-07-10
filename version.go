42050d2be8a79e19df01ff33daa31f2ea2673588
UMEZAWA Takeshi <takeshi-umezawa@cybozu.co.jp>
2024-07-10 13:45:15 +0900

    Bump version to 1.29.0-rc.1

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package cke
 
 // Version represents current cke version
-const Version = "1.28.6"
+const Version = "1.29.0-rc.1"
 
 // ConfigVersion represents the current configuration scheme
 // of how CKE constructs its Kubernetes cluster.
