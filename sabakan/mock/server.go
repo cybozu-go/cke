0f271251e2a5637b20980213c6b03b406076004c
YZ775 <yuzuki-mimura@cybozu.co.jp>
2024-09-20 04:57:50 +0000

    fix gqlgen

--- sabakan/mock/server.go
+++ sabakan/mock/server.go
@@ -10,7 +10,7 @@ import (
 // Server creates a mock server that implements sabakan GraphQL API.
 func Server() *httptest.Server {
 	h := handler.New(NewExecutableSchema(Config{
-		Resolvers: queryResolver{},
+		Resolvers: mockResolver{},
 	}))
 	h.AddTransport(transport.GET{})
 	h.AddTransport(transport.POST{})
